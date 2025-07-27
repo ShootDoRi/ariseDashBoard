b603335f7ea5d6806a2ac973bdfd1d324b72c572
ShootDoRi <kiseong@kakao.com>
2025-07-27 21:21:12 +0900

    49주차 데이터 업데이트2

--- src/store/common.js
+++ src/store/common.js
@@ -7,8 +7,9 @@ import localizedFormat from "dayjs/plugin/localizedFormat";
 dayjs.extend(localizedFormat);
 
 export const useCommonStore = defineStore("common", () => {
-  const endDate = "2025-07-24T08:30:00";
+  const endDate = "2025-07-31T08:30:00";
   const seasonEndDate = ref(dayjs(endDate).format("MM/DD HH:mm"));
+  const seasonEndDateText = ref("Season50 종료일");
 
   // search
   const searchState = reactive({
@@ -40,6 +41,7 @@ export const useCommonStore = defineStore("common", () => {
   return {
     endDate,
     seasonEndDate,
+    seasonEndDateText,
     searchState,
     modalState,
     columns,
