5dbb9029b51d026477cf9611bb82bbbedd57a9d6
KRDS-uiux <uiux@nia.or.kr>
2025-11-06 07:56:18 +0900

    update

--- src/store/common.js
+++ src/store/common.js
@@ -7,9 +7,9 @@ import localizedFormat from "dayjs/plugin/localizedFormat";
 dayjs.extend(localizedFormat);
 
 export const useCommonStore = defineStore("common", () => {
-  const endDate = "2025-10-30T08:30:00";
+  const endDate = "2025-11-07T08:30:00";
   const seasonEndDate = ref(dayjs(endDate).format("MM/DD HH:mm"));
-  const seasonEndDateText = ref("Season63 종료일");
+  const seasonEndDateText = ref("Season64 종료일");
 
   // search
   const searchState = reactive({
